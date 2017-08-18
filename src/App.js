9281109ae11efe694aee8ed0f47952f0c7040ebd
Leko <leko.noor@gmail.com>
2017-08-19 08:35:29 +0900

    :sparkles: Add basic UIs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Card, { CardActions, CardHeader, CardContent, CardMedia } from 'material-ui/Card'
+import List, {
+  ListItem,
+  ListItemAvatar,
+  ListItemIcon,
+  ListItemSecondaryAction,
+  ListItemText,
+} from 'material-ui/List'
+import Button from 'material-ui/Button'
+import Grid from 'material-ui/Grid'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Grid container justify='center' className="App">
+        <Grid item xs={12} sm={6}>
+          <Card style={{ width: '100%' }}>
+            <CardHeader
+              subheader='平成29年度 > 判別'
+              style={{ paddingTop: 5, paddingBottom: 5 }}
+            />
+            <img src="http://www.chusankan.net/blog/jimu/archives/assets_c/2009/10/middle_1239343266[1]-thumb-300x264-1840.jpg" alt=""/>
+            <CardContent>
+              これは何
+            </CardContent>
+            <List>
+              <ListItem button dense>
+                <ListItemText primary="コジュケイ" />
+              </ListItem>
+              <ListItem button dense>
+                <ListItemText primary="ムクドリ" />
+              </ListItem>
+              <ListItem button dense>
+                <ListItemText primary="ヨシガモ" />
+              </ListItem>
+              <ListItem button dense>
+                <ListItemText primary="ヒクイナ" />
+              </ListItem>
+            </List>
+          </Card>
+        </Grid>
+      </Grid>
+    )
   }
 }
 
-export default App;
+export default App
