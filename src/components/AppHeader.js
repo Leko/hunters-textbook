4e7289a5c4f98214792e327dfee7ee6b9e7c6764
Leko <leko.noor@gmail.com>
2017-08-24 23:10:39 +0900

    :sparkles: Add back to top link

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,9 +1,11 @@
 // @flow
 
 import React, { PureComponent } from 'react'
+import { Link } from 'react-router-dom'
 import Typography from 'material-ui/Typography'
 import AppBar from 'material-ui/AppBar'
 import Toolbar from 'material-ui/Toolbar'
+import Button from 'material-ui/Button'
 import IconButton from 'material-ui/IconButton'
 import MenuIcon from 'material-ui-icons/Menu'
 import '../styles/AppHeader.css'
@@ -16,9 +18,16 @@ export default class AppHeader extends PureComponent {
           <IconButton color='contrast' aria-label='Menu' onClick={this.props.onRequestSideBar}>
             <MenuIcon />
           </IconButton>
-          <Typography type='title' color='inherit' className='AppHeader__title'>
-            ポケット狩猟読本
-          </Typography>
+          <Button
+            dense
+            color='inherit'
+            component={Link}
+            to='/'
+          >
+            <Typography type='title' color='inherit' className='AppHeader__title'>
+              ポケット狩猟読本
+            </Typography>
+          </Button>
         </Toolbar>
       </AppBar>
     )
